b41b3534d7a080183c30e08cf420b3f568cfcab4
aCodeNPC <codenpc@163.com>
2024-11-08 16:00:03 +0800

    complete some linked list simple operate, and 100% cover unit test

--- linkedlist/linked_list.go
+++ linkedlist/linked_list.go
@@ -12,59 +12,134 @@
 
 package linkedlist
 
+import "fmt"
+
 type listNode struct {
 	val  int
 	next *listNode
 }
 
-// 判断两个链表是否相等, 依次判断元素值是否相等, 所有元素都相等意味着两个链表相等
-func isLinkedListEqual(l1, l2 *listNode) bool {
-	for l1 != nil && l2 != nil {
-		if l1.val != l2.val {
-			return false
-		}
-		l1 = l1.next
-		l2 = l2.next
-	}
+type list struct {
+	head *listNode
+	// count int
+	// cur   *listNode
+}
 
-	if l1 != nil {
+func (l *list) String() string {
+	s := ""
+	cur := l.head
+	for cur != nil {
+		if s == "" {
+			s += fmt.Sprintf("%d", cur.val)
+		} else {
+			s += fmt.Sprintf(" %d", cur.val)
+		}
+		cur = cur.next
+	}
+	return s
+}
+
+// 判断两个链表是否相等, 依次判断元素值是否相等, 所有元素都相等意味着两个链表相等
+func (l *list) isLinkedListEqual(l2 *list) bool {
+	if l == nil && l2 == nil {
+		return true
+	}
+	if l == nil && l2 != nil {
 		return false
 	}
-	if l2 != nil {
+	if l != nil && l2 == nil {
+		return false
+	}
+
+	cur1, cur2 := l.head, l2.head
+	for cur1 != nil && cur2 != nil {
+		if cur1.val != cur2.val {
+			return false
+		}
+		cur1 = cur1.next
+		cur2 = cur2.next
+	}
+
+	if cur1 != nil {
+		return false
+	}
+	if cur2 != nil {
 		return false
 	}
 	return true
 }
 
 // createLinkedListFromArray 创建一个链表, 返回链表头结点. 链表中的节点值就是数组元素依次赋值.
-func createLinkedListFromArray(arr []int) *listNode {
+func createLinkedListFromArray(arr []int) *list {
 	var head, cur *listNode
 	for i := range arr {
 		if head == nil {
 			head = &listNode{val: arr[i]}
 			cur = head
+
 		} else {
 			cur.next = &listNode{val: arr[i]}
 			cur = cur.next
 		}
 	}
-	return head
+
+	l := &list{head: head}
+	return l
 }
 
 // insertNodeHead 1. 在链表表头插入结点：空链表，只有一个节点的链表，有两个节点的链表
-func insertNodeHead(list *listNode, val int) *listNode {
-	if list == nil {
-		return &listNode{val: val}
+func (l *list) insertNodeHead(val int) {
+	if l.head == nil {
+		l.head = &listNode{val: val}
+		return
 	}
 
 	newNode := &listNode{val: val}
-	newNode.next = list
-	return newNode
+	newNode.next = l.head
+	l.head = newNode
 }
 
-// 2. 在链表表尾插入结点：空链表，只有一个节点的链表，有两个节点的链表
-// 3. 在链表中删除表头节点：空链表，只有一个节点的链表，有两个节点的链表
+// insertNodeTail 2. 在链表表尾插入结点：空链表，只有一个节点的链表，有两个节点的链表
+func (l *list) insertNodeTail(val int) {
+	if l.head == nil {
+		l.head = &listNode{val: val}
+		return
+	}
+
+	cur := l.head
+	for cur.next != nil {
+		cur = cur.next
+	}
+	cur.next = &listNode{val: val}
+}
+
+// delNodeHead 3. 在链表中删除表头节点：空链表，只有一个节点的链表，有两个节点的链表
+func (l *list) delNodeHead() {
+	if l.head == nil {
+		return
+	}
+	l.head = l.head.next
+}
+
 // 4. 在链表中删除表尾节点：空链表，只有一个节点的链表，有两个节点的链表
+func (l *list) delNodeTail() {
+	if l.head == nil {
+		return
+	}
+
+	if l.head.next == nil {
+		l.head = nil
+		return
+	}
+
+	cur := l.head
+	for cur.next.next != nil {
+		cur = cur.next
+	}
+
+	cur.next = nil
+}
+
 // 3. 单链表反转
 // 4. 链表中环的检测
 // 5. 有序链表合并
--- linkedlist/linked_list_test.go
+++ linkedlist/linked_list_test.go
@@ -1,52 +1,10 @@
 package linkedlist
 
-import "testing"
+import (
+	"testing"
 
-func TestIsLinkedListEqual(t *testing.T) {
-	e1 := isLinkedListEqual(nil, nil)
-	if !e1 {
-		t.Errorf("case1: two nil linkedlist not equal?")
-	}
-
-	l1 := &listNode{}
-	l2 := l1
-	e2 := isLinkedListEqual(l1, l2)
-	if !e2 {
-		t.Errorf("case2: two same linkedlist not equal?")
-	}
-
-	l1 = &listNode{val: 1}
-	l2 = l1
-	e3 := isLinkedListEqual(l1, l2)
-	if !e3 {
-		t.Errorf("case3: two same linkedlist not equal?")
-	}
-
-	l1 = &listNode{val: 1}
-	l2 = &listNode{val: 1}
-	l2.next = &listNode{val: 2}
-	e4 := isLinkedListEqual(l1, l2)
-	if e4 {
-		t.Errorf("case4: two not same linkedlist equal?")
-	}
-
-	l1 = &listNode{val: 1}
-	l1.next = &listNode{val: 2}
-	l2 = &listNode{val: 1}
-	e5 := isLinkedListEqual(l1, l2)
-	if e5 {
-		t.Errorf("case5: two not same linkedlist equal?")
-	}
-
-	l1 = &listNode{val: 5}
-	l1.next = &listNode{val: 2}
-	l2 = &listNode{val: 2}
-	l2.next = &listNode{val: 5}
-	e6 := isLinkedListEqual(l1, l2)
-	if e6 {
-		t.Errorf("case6: two not same linkedlist equal?")
-	}
-}
+	"github.com/stretchr/testify/assert"
+)
 
 // go test -v github/aCodeNPC/algo/linkedlist  包级测试
 // go test -v -run '^Test' github/aCodeNPC/algo/linkedlist 指定函数名以Test开头的
@@ -54,49 +12,216 @@ func TestIsLinkedListEqual(t *testing.T) {
 // go test -v -run '^Test' github/aCodeNPC/algo/linkedlist -coverprofile=coverage.out
 // go tool cover -func=coverage.out 本地查看覆盖率
 // go tool cover -html=coverage.out 打开网页显示详细的覆盖情况
+
+func TestIsLinkedListEqual(t *testing.T) {
+	l1 := new(list)
+	e1 := l1.isLinkedListEqual(nil)
+	if e1 {
+		t.Errorf("case1: empty list equal nil list")
+	}
+
+	l1 = new(list)
+	l2 := l1
+	e2 := l1.isLinkedListEqual(l2)
+	if !e2 {
+		t.Errorf("case2: two same linkedlist not equal?")
+	}
+
+	l1 = new(list)
+	l1.head = &listNode{val: 1}
+	l2 = l1
+	e3 := l1.isLinkedListEqual(l2)
+	if !e3 {
+		t.Errorf("case3: two same linkedlist not equal?")
+	}
+
+	l1 = new(list)
+	l1.head = &listNode{val: 1}
+	l2 = new(list)
+	l2.head = &listNode{val: 1}
+	l2.head.next = &listNode{val: 2}
+	e4 := l1.isLinkedListEqual(l2)
+	if e4 {
+		t.Errorf("case4: two not same linkedlist equal?")
+	}
+
+	l1 = new(list)
+	l1.head = &listNode{val: 1}
+	l1.head.next = &listNode{val: 2}
+	l2 = new(list)
+	l2.head = &listNode{val: 1}
+
+	e5 := l1.isLinkedListEqual(l2)
+	if e5 {
+		t.Errorf("case5: two not same linkedlist equal?")
+	}
+
+	l1 = createLinkedListFromArray([]int{5, 2})
+	l2 = createLinkedListFromArray([]int{2, 5})
+	e6 := l1.isLinkedListEqual(l2)
+	if e6 {
+		t.Errorf("case6: two not same linkedlist equal?")
+	}
+
+	var l *list
+	e7 := l.isLinkedListEqual(nil)
+	assert.Equal(t, true, e7)
+
+	e8 := l.isLinkedListEqual(l2)
+	assert.NotEqual(t, true, e8)
+
+}
+
 func TestCreateLinkedListFromArray(t *testing.T) {
 	l := createLinkedListFromArray(nil)
-	if l != nil {
-		t.Errorf("case1: nil array create not nil linkedlist:%v", l)
+	if l.head != nil {
+		t.Errorf("nil arr create not nil list.head")
 	}
+	assert.Equal(t, nil, nil)
 
 	l1 := createLinkedListFromArray([]int{})
-	if l1 != nil {
-		t.Errorf("case2: empty array create not nil linkedlist:%v", l)
+	if l1.head != nil {
+		t.Errorf("empty arr create not nil list.head")
 	}
+	// assert.Equal(t, nil, l1)
 
 	l2 := createLinkedListFromArray([]int{1})
-	l2c := &listNode{val: 1}
-	if !isLinkedListEqual(l2, l2c) {
+	l2c := new(list)
+	l2c.head = &listNode{val: 1}
+	if !l2.isLinkedListEqual(l2c) {
 		t.Errorf("case3: one element array create linkedlist:%v %v", l2, l2c)
 	}
 
 	l3 := createLinkedListFromArray([]int{1, 2})
-	l3c := &listNode{val: 1}
-	l3c.next = &listNode{val: 2}
-	if !isLinkedListEqual(l3, l3c) {
+	l3c := new(list)
+	l3c.head = &listNode{val: 1}
+	l3c.head.next = &listNode{val: 2}
+	if !l3.isLinkedListEqual(l3c) {
 		t.Errorf("case4: two element array create linkedlist:%v %v", l3, l3c)
 	}
 }
 
-func TestInsertNodeHead(t *testing.T) {
-	cases := []struct {
-		name   string
-		arr    []int
-		v      int
-		expect []int
+func TestString(t *testing.T) {
+	tests := []struct {
+		name  string
+		input []int
+		want  string
 	}{
-		{name: "case1", arr: nil, v: 1, expect: []int{1}},
-		{name: "case2", arr: []int{1}, v: 2, expect: []int{2, 1}},
-		{name: "case3", arr: []int{1, 2}, v: 3, expect: []int{3, 1, 2}},
+		{"test1", nil, ""},
+		{"test2", []int{}, ""},
+		{"test3", []int{1}, "1"},
+		{"test4", []int{1, 2}, "1 2"},
+		{"test5", []int{1, 2, 3}, "1 2 3"},
 	}
 
-	for _, c := range cases {
-		l := createLinkedListFromArray(c.arr)
-		ln := insertNodeHead(l, c.v)
-		lc := createLinkedListFromArray(c.expect)
-		if !isLinkedListEqual(ln, lc) {
-			t.Errorf("%s: input:%v expect:%v ln:%v lc:%v", c.name, c.arr, c.expect, ln, lc)
-		}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			l := createLinkedListFromArray(tt.input)
+			s := l.String()
+			assert.Equal(t, tt.want, s)
+		})
+	}
+}
+
+func TestInsertNodeHead(t *testing.T) {
+	tests := []struct {
+		name   string
+		input  []int
+		insert int
+		want   []int
+	}{
+		{name: "case1", input: nil, insert: 1, want: []int{1}},
+		{name: "case2", input: []int{}, insert: 1, want: []int{1}},
+		{name: "case3", input: []int{1}, insert: 2, want: []int{2, 1}},
+		{name: "case4", input: []int{1, 2}, insert: 3, want: []int{3, 1, 2}},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			li := createLinkedListFromArray(tt.input)
+			li.insertNodeHead(tt.insert)
+			lw := createLinkedListFromArray(tt.want)
+			if !li.isLinkedListEqual(lw) {
+				t.Errorf("%s: input:%v expect:%v li:%v lw:%v", tt.name, tt.input, tt.want, li, lw)
+			}
+		})
+	}
+}
+
+func TestInsertNodeTail(t *testing.T) {
+	tests := []struct {
+		name   string
+		input  []int
+		insert int
+		want   []int
+	}{
+		{"test1", nil, 1, []int{1}},
+		{"test2", []int{}, 1, []int{1}},
+		{"test3", []int{1}, 2, []int{1, 2}},
+		{"test4", []int{1, 2}, 3, []int{1, 2, 3}},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			li := createLinkedListFromArray(tt.input)
+			li.insertNodeTail(tt.insert)
+			lw := createLinkedListFromArray(tt.want)
+			if !li.isLinkedListEqual(lw) {
+				t.Errorf("%s: input:%v expect:%v li:%v lw:%v", tt.name, tt.input, tt.want, li, lw)
+			}
+		})
+
+	}
+}
+
+func TestDelNodeHead(t *testing.T) {
+	tests := []struct {
+		name  string
+		input []int
+		want  []int
+	}{
+		{"test1", nil, nil},
+		{"test2", []int{}, nil},
+		{"test3", []int{1}, nil},
+		{"test4", []int{1, 2}, []int{2}},
+		{"test5", []int{1, 2, 3}, []int{2, 3}},
+		{"test5", []int{1, 2, 3, 4}, []int{2, 3, 4}},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			li := createLinkedListFromArray(tt.input)
+			li.delNodeHead()
+			lw := createLinkedListFromArray(tt.want)
+			if !li.isLinkedListEqual(lw) {
+				t.Errorf("li:%v != lw:%v", li, lw)
+			}
+		})
+	}
+}
+
+func TestDelNodeTail(t *testing.T) {
+	tests := []struct {
+		name  string
+		input []int
+		want  []int
+	}{
+		{"test1", nil, nil},
+		{"test2", []int{}, nil},
+		{"test3", []int{1}, nil},
+		{"test4", []int{1, 2}, []int{1}},
+		{"test5", []int{1, 2, 3}, []int{1, 2}},
+		{"test5", []int{1, 2, 3, 4}, []int{1, 2, 3}},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			li := createLinkedListFromArray(tt.input)
+			li.delNodeTail()
+			lw := createLinkedListFromArray(tt.want)
+			if !li.isLinkedListEqual(lw) {
+				t.Errorf("li:%v != lw:%v", li, lw)
+			}
+		})
 	}
 }
